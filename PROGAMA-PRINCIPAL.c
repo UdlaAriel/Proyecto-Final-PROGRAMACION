b7249cd743b8deca141024f174cc045bda90b3ab
UdlaAriel <ariel.anchapaxi@gmail.com>
2023-07-01 23:53:50 -0500

    Se añadieron la definición de dos funciones, la primera cantidadNomina y la segunda funcionConsulta. De este modo se determinó la cantidad de personas dentro de la nómina, para así realizar las modificaciones correspondientes al fichero.

--- PROGAMA-PRINCIPAL.c
+++ PROGAMA-PRINCIPAL.c
@@ -5,21 +5,25 @@
 #define MAX 50
 #define MIN 25
 
-typedef struct informacionPersonal{
+void funcionConsulta (); //Lee los datos dentro del fichero y los imprime en pantalla
+void funcionActualizacion (); //Permite cambiar o acutalizar los datos del fichero
+void cantidadNomina (int *filas); //Devuelve la cantidad de personas dentro de la nómina
+
+int filas = 0; //numero de filas
+char archivo[] = "BASE_DE_DATOS.csv"; //Nombre de archivo que va a ser utilizado
+typedef struct informacionPersonal{ //Estructura que almacenará la información de cada persona en nómina
     char ID[MAX];
     char Nombre[MAX];
     char Cargo[MAX];
     char Salario[MIN];
     char SalarioTOTAL[MIN];
-};
-
-void funcionConsulta ();
-void funcionActualizacion ();
+}datos[];
 
 int main ()
 {
+    int *pointer = &filas; 
     int vflag = 1, opc = 0;
-    char *opciones[] = {
+    char *opciones[3] = {
         "1. Ver informacion de nomina",
         "2. Actualizar nomina",
         "3. Salir"
@@ -28,11 +32,13 @@ int main ()
     printf("\n\nIniciando..");
     Sleep(200);
 
-    do
-    {
-        printf("MANEJO DE DATOS\n%s\n%s\n%s\bA continuación digite la opción que desee ejecutar >> ",*opciones[0],*opciones[1],*opciones[2]);
-        scanf("%d",&opc);
+    while (vflag != 0){
 
+        cantidadNomina(pointer);
+        printf("Cantidad de personas en nomina %d", filas);
+        printf("\nPROGRAMA DE MANEJO DE DATOS\n%s\n%s\n%s\nA continuacion digita la opcion que desee ejecutar >> ",opciones[0],opciones[1],opciones[2]);
+        scanf("%d",&opc);
+        
         switch (opc)
         {
         case 1: funcionConsulta(); break; //Ver información de nómina
@@ -44,8 +50,32 @@ int main ()
             break;
             }
         }
-
-    } while (vflag);    //Si vflag es igual a 0, el programa se cerrará
-    printf("Ha cerrado el programa.");
+    }   //Si vflag es igual a 0, el programa se cerrará
+    printf("\nHa cerrado el programa.");
     return 0;
 }
+
+void cantidadNomina (int *pointer){ //Funcion para determinar la cantidad de personas en nomina leyendo cada caracter de salto de linea
+    FILE *archivoBASE;
+    if((archivoBASE = fopen(archivo,"r"))==NULL) printf("\nLo sentimos, existe un error al abrir el archivo %s",archivo);
+    else printf("\nArchivo abierto correctamente.\n");
+
+    while (!feof(archivoBASE)){
+        if(fgetc(archivoBASE)=='\n') 
+            ++*pointer;
+    }
+    *pointer -= 1;
+    fclose(archivoBASE);
+}
+
+void funcionConsulta (){
+    FILE *archivoBASE;
+    if((archivoBASE = fopen(archivo,"r"))==NULL) printf("Lo sentimos, existe un error al abrir el archivo %s",archivo);
+    else printf("Archivo abierto correctamente.");
+    fclose(archivoBASE);
+}
+
+void funcionActualizacion (){
+
+}
+
