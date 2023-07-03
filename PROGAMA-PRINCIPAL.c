f692818f7c3db51683a16faa0231e43a4ae5c920
Ariel Mateo Anchapaxi Moreta <130407481+UdlaAriel@users.noreply.github.com>
2023-07-03 15:18:19 -0500

    Se redujo y optimizó el uso de varias funciones.
    Se define una funcion para identificar el maximo de personas dentro de la NOMINA. Así mismo, se inicializa un struct de memoria dinámica.

--- PROGAMA-PRINCIPAL.c
+++ PROGAMA-PRINCIPAL.c
@@ -7,7 +7,7 @@
 #define MIN 50
 
 char archivo[] = "BASE_DE_DATOS.csv"; //Nombre de archivo que va a ser utilizado
-typedef struct NOMINA{ //Estructura que almacenará la información de cada persona en nómina
+typedef struct{ //Estructura que almacenará la información de cada persona en nómina
     char ID[MIN];
     char Nombre[MIN];
     char Cargo[MIN];
@@ -18,47 +18,33 @@ typedef struct NOMINA{ //Estructura que almacenará la información de cada pers
 
 int funcionActualizacion (); //Permite cambiar o acutalizar los datos del fichero
 void funcionConsulta (int personasNomina, informacion *DATA); //Lee los datos dentro del fichero y los imprime en pantalla
-void cantidadNomina (int *pointer1); //Devuelve la cantidad de personas dentro de la nómina, además de idenficar el numero de secciones
-void separadorDatos (char linea[1000]);
+int cantidadNomina (); //Devuelve la cantidad de personas dentro de la nómina, además de idenficar el numero de secciones
 
 int main ()
 {
-    int vflag = 1, opc = 0;
-    char *opciones[3] = {
-        "1. Ver informacion de nomina",
-        "2. Actualizar nomina",
-        "3. Salir"
-    };
+    int opc = 0;
 
-    //printf("\nIniciando..\n");
-    //Sleep(200);s
+    while (1){
+        int personasNomina = cantidadNomina(); //numero de personas en nómina
 
-    while (vflag != 0){
-        int personasNomina = 0; //numero de personas en nómina
-        int *pointer1 = &personasNomina; //Puntero que modifica de forma dinámica el número de personas en nómina
-
-        informacion *DATA;  //Definición de struct para almacenar la información de cadad miembro dentro de la nómina
-        DATA = (informacion*) malloc(personasNomina * sizeof(informacion));
+        informacion *DATA = calloc(personasNomina, sizeof(informacion));  //Definición de struct para almacenar la información de cadad miembro dentro de la nómina
     
-        if(DATA==NULL){ //Comprobación de asignación de memoria al puntero SIZE de tipo informacion
+        if(!DATA){ //Comprobación de asignación de memoria al puntero SIZE de tipo informacion
             printf("\nError al asignar memoria dinamicamente.\n");
             return 1;
         }
 
-        cantidadNomina(pointer1);
         printf("Cantidad de personas en NOMINA %d", personasNomina);
-        printf("\n\nPROGRAMA DE MANEJO DE DATOS\n%s\n%s\n%s\nA continuacion digita la opcion que desee ejecutar >> ",opciones[0],opciones[1],opciones[2]);
+        printf("\n\nPROGRAMA DE MANEJO DE DATOS\n1. Ver informacion de nomina\n2. Actualizar nomina\n3. Salir\nA continuacion digita la opcion que desee ejecutar >> ");
         scanf("%d",&opc);
         
         switch (opc)
         {
         case 1: funcionConsulta(personasNomina, DATA); break;//Ver información de nómina
         case 2: funcionActualizacion(); break; //Modificar o actualizar la información de nómina          
-        case 3: system("cls"); vflag = 0; break;//Salir del programa
+        case 3: return 0;//Salir del programa
         default: {
             system("cls");
-            printf("Ingrese una OPCION VALIDA.\n"); 
-            scanf("%d", &opc); 
             break;
             }
         }
@@ -69,31 +55,19 @@ int main ()
     return 0;
 }
 
-void cantidadNomina (int *pointer1){ //Funcion para determinar la cantidad de personas en nomina leyendo cada caracter de salto de linea
-    *pointer1 = 0; //Inicializar el valor de la variable a la que pointer1 está apuntando
+int cantidadNomina(){ //Funcion para determinar la cantidad de personas en nomina leyendo cada caracter de salto de linea
+    int cantidad = 0; //Inicializar el valor de la variable que almacena el valor de personas en nomina
     FILE *archivoBASE;
     if((archivoBASE = fopen(archivo,"r"))==NULL) printf("\nLo sentimos, existe un error al abrir el archivo %s",archivo);
     else printf("\nArchivo abierto correctamente.\n");
 
     while (!feof(archivoBASE)){
-        if(fgetc(archivoBASE)=='\n') ++*pointer1;
+        if(fgetc(archivoBASE)=='\n') cantidad++;
     }
      //Asigna el valor del total de personas dentro de nomina a la variable global filas
     fclose(archivoBASE);
-}
 
-void separadorDatos (char linea[1000]){
-    char* token;
-    int count;
-    token = strtok(linea,",");
-    //strcpy(DATA->ID,token);
-
-    if(token!=NULL){
-        while(token!=NULL){
-            printf("%s\n", token);
-            token = strtok(NULL,",");
-        }
-    }
+    return cantidad;
 }
 
 void funcionConsulta (int personasNomina, informacion *DATA){
@@ -111,7 +85,6 @@ void funcionConsulta (int personasNomina, informacion *DATA){
     for(int i=0;i<personasNomina;i++){
         fgets(linea,sizeof(linea),archivoBASE_Lectura);
         printf("%s", linea);
-        separadorDatos(linea);
     }   
     
     fclose(archivoBASE_Lectura);
