bc28b81be31e34e1e6cff93c342ce0e272115e3c
UdlaAriel <ariel.anchapaxi@gmail.com>
2023-07-03 11:33:58 -0500

    Revisión de sintaxis en la función funcionConsulta y creación de función separadorDatos.

--- PROGAMA-PRINCIPAL.c
+++ PROGAMA-PRINCIPAL.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 #include <conio.h>
 #include <windows.h>
 
 #define MAX 100
-#define MEDIUM 50
-#define MIN 25
+#define MIN 50
 
 char archivo[] = "BASE_DE_DATOS.csv"; //Nombre de archivo que va a ser utilizado
 typedef struct NOMINA{ //Estructura que almacenará la información de cada persona en nómina
-    char ID[MEDIUM];
-    char Nombre[MEDIUM];
-    char Cargo[MEDIUM];
-    int Salario[MIN];
-    int SalarioTOTAL[MIN];
+    char ID[MIN];
+    char Nombre[MIN];
+    char Cargo[MIN];
+    char HorasExtra[10]; //Definición como caracter de datos tipo int, para copiar el contenido dek fichero y luego transformnarlo
+    char Salario[10];
+    char SalarioTOTAL[10];
 }informacion;
 
 int funcionActualizacion (); //Permite cambiar o acutalizar los datos del fichero
-void funcionConsulta (int personasNomina); //Lee los datos dentro del fichero y los imprime en pantalla
+void funcionConsulta (int personasNomina, informacion *DATA); //Lee los datos dentro del fichero y los imprime en pantalla
 void cantidadNomina (int *pointer1); //Devuelve la cantidad de personas dentro de la nómina, además de idenficar el numero de secciones
+void separadorDatos (char linea[1000]);
 
 int main ()
 {
@@ -28,8 +30,8 @@ int main ()
         "3. Salir"
     };
 
-    printf("\n\nIniciando..");
-    //Sleep(200);
+    //printf("\nIniciando..\n");
+    //Sleep(200);s
 
     while (vflag != 0){
         int personasNomina = 0; //numero de personas en nómina
@@ -50,21 +52,25 @@ int main ()
         
         switch (opc)
         {
-        case 1: funcionConsulta(personasNomina); break;//Ver información de nómina
+        case 1: funcionConsulta(personasNomina, DATA); break;//Ver información de nómina
         case 2: funcionActualizacion(); break; //Modificar o actualizar la información de nómina          
         case 3: system("cls"); vflag = 0; break;//Salir del programa
         default: {
-            printf("Ingrese una opción valida.\n"); 
+            system("cls");
+            printf("Ingrese una OPCION VALIDA.\n"); 
             scanf("%d", &opc); 
             break;
             }
         }
+        
+        free(DATA);
     }   //Si vflag es igual a 0, el programa se cerrará
     printf("\nHa cerrado el programa.");
     return 0;
 }
 
 void cantidadNomina (int *pointer1){ //Funcion para determinar la cantidad de personas en nomina leyendo cada caracter de salto de linea
+    *pointer1 = 0; //Inicializar el valor de la variable a la que pointer1 está apuntando
     FILE *archivoBASE;
     if((archivoBASE = fopen(archivo,"r"))==NULL) printf("\nLo sentimos, existe un error al abrir el archivo %s",archivo);
     else printf("\nArchivo abierto correctamente.\n");
@@ -72,30 +78,41 @@ void cantidadNomina (int *pointer1){ //Funcion para determinar la cantidad de pe
     while (!feof(archivoBASE)){
         if(fgetc(archivoBASE)=='\n') ++*pointer1;
     }
-    
-    *pointer1 -= 1; //Asigna el valor del total de personas dentro de nomina a la variable global filas
+     //Asigna el valor del total de personas dentro de nomina a la variable global filas
     fclose(archivoBASE);
 }
 
-void funcionConsulta (int personasNomina){
+void separadorDatos (char linea[1000]){
+    char* token;
+    int count;
+    token = strtok(linea,",");
+    //strcpy(DATA->ID,token);
+
+    if(token!=NULL){
+        while(token!=NULL){
+            printf("%s\n", token);
+            token = strtok(NULL,",");
+        }
+    }
+}
+
+void funcionConsulta (int personasNomina, informacion *DATA){
     system("cls");
 
-    char linea[1000], *token;
+    char linea[1000];
+
     FILE *archivoBASE_Lectura;
     if((archivoBASE_Lectura = fopen(archivo,"rt"))==NULL) printf("Lo sentimos, existe un error al abrir el archivo %s",archivo);
-    else printf("Archivo abierto correctamente.");
+    else printf("Archivo %s abierto correctamente.",archivo);
 
-    printf("\nID\tNOMBRE\tPUESTO\tSALARIO MENSUAL\tH.E MENSUALES\tSALARIO TOTAL");
-    fgets(linea,sizeof(linea),archivoBASE_Lectura);
+    printf("\nID\tNOMBRE\tPUESTO\tSALARIO MENSUAL\tH.E MENSUALES\tSALARIO TOTAL\n");
+    fgets(linea,sizeof(linea),archivoBASE_Lectura);//Almacena el contenido de la primera linea del fichero
 
     for(int i=0;i<personasNomina;i++){
         fgets(linea,sizeof(linea),archivoBASE_Lectura);
         printf("%s", linea);
-
-        token = strtok(linea,",");
-
-
-    }
+        separadorDatos(linea);
+    }   
     
     fclose(archivoBASE_Lectura);
 }
